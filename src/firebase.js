f47e6942068fab3271a12ce31e1785e61fad27ec
Sharad Chandra Reddy <97039791+SCR01@users.noreply.github.com>
2025-09-02 20:38:14 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
