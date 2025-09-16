cd11d9044f149252ae3fd5339b09fa96e546342e
zaidxGithub <zaidxcodes@gmail.com>
2025-09-16 13:44:42 +0530

    Fix navbar and button layout: update margins, paddings, borders, height, width, spacing, and bottom border; make fully responsive across all screens; adjust border-radius for login and product buttons; resolve Tailwind CSS layout bugs

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
+
+  // Updated the button borders
   return (
     <button
       id={id}
-      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full  px-7 py-3 text-black ${containerClass}`}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-md  px-4 py-2 text-black ${containerClass}`}
     >
       {leftIcon}
-      <span className="relative incline-flex overflow-hidden font-nippo-light text-xs uppercase">
+      <span className="relative incline-flex overflow-hidden font-nippo-light text-sm uppercase">
         <div>
           <b>{title}</b>
         </div>
